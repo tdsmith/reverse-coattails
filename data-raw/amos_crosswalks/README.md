14df33d9a06bb21a8111e510a7f0ebf097085fc3
Tim D. Smith <git@tim-smith.us>
2022-05-24 12:49:31 -0700

    Pull in 2010-2020 crosswalks

---
+++ data-raw/amos_crosswalks/README.md
@@ -0,0 +1,3 @@
+https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/T9VMJO
+
+Relates 2010 census blocks to 2020 census blocks using land use data to refine the estimate of where people live.
--- extract.py
+++ extract.py
@@ -1,4 +1,8 @@
+import csv
+import io
+import zipfile
 from pathlib import Path
+from typing import List
 
 import pandas as pd
 
@@ -47,3 +51,43 @@ def medsl_state() -> pd.DataFrame:
 def medsl_house() -> pd.DataFrame:
     path = medsl_path / "HOUSE/HOUSE_precinct_general.zip"
     return pd.read_csv(path, dtype=medsl_dtypes)
+
+def read_crosswalks(state_fips: List[str] = []) -> pd.DataFrame:
+    """Read crosswalks into a tabular format.
+
+    Crosswalks are specified in a format like:
+        GEOID20,GEOID10,x
+    where x is the fraction of the GEOID10 block that GEOID20 occupies.
+    (The official datasets use area; the VEST crosswalks by Brian Amos which we
+    use here use a land-use database to try and weight by residential density.)
+
+    Of course, a GEOID20 may overlap more than one GEOID10! In the dataset,
+    this looks like
+        GEOID20,GEOID10_a,x,GEOID10_b,y
+
+    This function produces a table with one row per relationship instead, like:
+        GEOID20,GEOID10_a,x
+        GEOID20,GEOID10_b,y
+    """
+    zf_path = (root / "data-raw/amos_crosswalks/block10block20_crosswalks.zip")
+    zf = zipfile.ZipFile(zf_path)
+
+    csvs = [name for name in zf.namelist() if name.endswith(".csv")]
+    int_fips = [int(i) for i in state_fips]
+    records = []
+
+    for csv_filename in csvs:
+        state = csv_filename.rsplit(".", maxsplit=1)[0].rsplit("_", maxsplit=1)[1]
+        if int_fips:
+            if int(state) not in int_fips:
+                continue
+        f = zf.open(csv_filename, "r")
+        wrapper = io.TextIOWrapper(f, "ascii", newline="")
+        reader = csv.reader(wrapper)
+        for line in reader:
+            geoid20 = line[0]
+            for i in range(1, len(line), 2):
+                geoid10, weight = line[i], line[i+1]
+                records.append((state, geoid20, geoid10, weight))
+        f.close()
+    return pd.DataFrame(records, columns=["STATE", "GEOID20", "GEOID10", "weight"])
